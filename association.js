f768615bfac65896f520c68cba1e2a644261cf57
Kent B Carlyle II <kentbcarlyle@gmail.com>
2018-11-08 13:12:50 -0500

    remove extraneous

--- association.js
+++ association.js
@@ -1,19 +1,14 @@
 const sequelize = require("./db");
 
 var User = sequelize.import("./models/User");
-var Admin = sequelize.import("./models/Admin");
 var Item = sequelize.import("./models/Item");
 var Comment = sequelize.import("./models/Comment");
-var RedBadgeForum = sequelize.import("./models/RedBadgeForum");
 
 User.hasMany(Item);
 Item.belongsTo(User);
 User.hasMany(Comment);
 Comment.belongsTo(User);
-// RedBadgeForum.hasMany(Comment);
-// Comment.belongsTo(RedBadgeForum);
 
 sequelize.sync();
 
 // sequelize.sync({ force: true })
-
